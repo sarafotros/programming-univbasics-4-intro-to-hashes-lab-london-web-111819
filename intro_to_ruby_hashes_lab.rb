222b9b2dad5871e19b0e0e75c89e4476cab94939
Sara Fotros <sarafotros1@yahoo.com>
2019-11-12 09:16:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,14 +21,14 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  if my_hash_creator[key]
+  if my_hash_creator[:key]
     puts "it has value"
   else
-    my_hash_creator[key] = "valu"
+    my_hash_creator[:key] = "value"
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  
 end
 
 def update_counting_hash(hash, key)
